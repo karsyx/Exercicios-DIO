2c4139ec27e289621e7279356172064ba7ebb092
Karsyx <karsyx@kali-dell.kali-dell.com>
2021-08-16 10:45:48 -0300

    ultimo exemplo de funções avançadas adicionado

--- funcoes_avancadas/js/tretamentosExcecoes.js
+++ funcoes_avancadas/js/tretamentosExcecoes.js
@@ -1,6 +1,25 @@
 // como indentificar os erros
+class CustomError extends Error {
+    constructor({ message, data }){
+        super(message);
+        this.data = data;
+    }
+}
 
-console.log(name);
-const name = 'Celso Henrique';
+try{
+    const name = 'Celso Henrique';
+
+    const myError = new CustomError({
+        message: 'Custom message on custom error',
+        data: {
+            type: 'Server error'
+        }
+    });
+
+    throw myError;
+} catch (err) {
+    console.log(err);
+    console.log(err.data);
+}
 
 console.log('Keep going...');
