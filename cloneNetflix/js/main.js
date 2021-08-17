b0b2045e84e762b72f6d9d0ca8f32c50dbd03cff
karsyx <karsyx@kaliPC.kali-pc.com>
2021-08-17 06:40:02 -0300

    Exercicio proposto para criar um 'clone' do site Netflix

---
+++ cloneNetflix/js/main.js
@@ -0,0 +1,19 @@
+function homeRedirect(){
+    window.location.href = "gatoflix.html";
+}
+
+function buttonPlay(){
+    window.location.href = "payment.html";
+}
+
+function detailsInterstellar(){
+    window.open("https://www.imdb.com/title/tt0816692/");
+}
+
+function detailsBlack(){
+    window.open("https://www.imdb.com/title/tt3480822/?ref_=nv_sr_srsg_0");
+}
+
+function detailsLoki(){
+    window.open("https://www.imdb.com/title/tt9140554/");
+}
